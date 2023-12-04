5609dda01dd0d1a6b9a6e40f8edea8ec9a81e2d7
Your Name <you@example.com>
2023-12-04 16:22:32 +0200

    change theme and creat notes view

---
+++ lib/view/notes_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class NotesView extends StatelessWidget {
+  static const String routeName = 'Notes View';
+  const NotesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
