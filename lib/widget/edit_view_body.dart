c92f71364b3f03443a9832d94e600005d829464c
Your Name <you@example.com>
2023-12-05 20:57:03 +0200

    edit note view

--- lib/widget/edit_view_body.dart
+++ lib/widget/edit_view_body.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/widget/custom_text_from_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 40,
+          ),
+          CustomTextFormField(labelText: 'Titel'),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextFormField(labelText: 'Content', maxLine: 5),
+        ],
+      ),
     );
   }
 }
