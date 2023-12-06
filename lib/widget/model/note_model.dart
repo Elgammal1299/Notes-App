5b414e6e415a02ee2d926366a8dbc975f8b7c7ae
Your Name <you@example.com>
2023-12-06 07:16:05 +0200

    creat note model

---
+++ lib/widget/model/note_model.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class NoteModel {
+  final String titel;
+  final String supTitel;
+  final String data;
+  final int color;
+
+  NoteModel({
+    required this.data,
+    required this.titel,
+    required this.supTitel,
+    required this.color,
+  });
+}
